f485376f708112515d20e86d71cbda0b6e1f88e7
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-06-17 18:01:13 -0700

    Added task model and admin display

--- taskmate/taskmate/settings.py
+++ taskmate/taskmate/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
 
-STATIC_URL = '/templates/'
+STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'todolist_app/templates')
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
--- taskmate/todolist_app/models.py
+++ taskmate/todolist_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class TaskList(models.Model):
+    task = models.CharField(max_length=300)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        if (self.done == True):
+            fin = "Done"
+        else:
+            fin = "Pending"
+
+        return self.task + " - " + fin 
