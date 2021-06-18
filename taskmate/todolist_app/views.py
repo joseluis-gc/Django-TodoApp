9e88be405ec145a7893996fb43de2757c16686b7
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-06-18 12:09:37 -0700

    Added delete functionalities

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -20,6 +20,13 @@ def todolist(request):
         return render(request, 'todolist.html', {'all_tasks':all_tasks})    
 
 
+def delete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+
+    return redirect('todolist')
+
+
 
 def contact(request):
     context = {'welcome_text': "Contact Us"}
