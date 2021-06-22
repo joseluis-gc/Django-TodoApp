b6eb7697904252899644dc6cdd92e2127ae82ecc
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-06-21 17:57:13 -0700

    added messages to register form

--- taskmate/users_app/views.py
+++ taskmate/users_app/views.py
@@ -1,7 +1,22 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 # Create your views here.
 def register(request):
-    return HttpResponse('Uses App Working.')
+    #return HttpResponse('Uses App Working.')
+
+    if(request.method == "POST"):
+        register_form = UserCreationForm(request.POST)
+        if(register_form.is_valid()):
+            register_form.save()
+            messages.success(request, ("Account created successfully!"))
+            return redirect('register')
+
+    else:
+        register_form = UserCreationForm()
+    return render(request, 'register.html', {'register_form': register_form})
+    
